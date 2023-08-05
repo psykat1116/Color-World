d442b976a5900dd72a38f0b58de77f56aa23d369
Saikat Samanta <95569573+psykat1116@users.noreply.github.com>
2023-08-05 22:18:49 +0530

    Add Colors

--- themeScript.js
+++ themeScript.js
@@ -25,6 +25,70 @@ themeColors.forEach((theme) => {
             })
             numOfColors.innerHTML = `Number of Palletes is ${halloween.length}`;
         }
+        else if (th == "Christmas") {
+            christmas.forEach((color) => {
+                let li = document.createElement("li");
+                li.style.width = "230px";
+                li.style.height = "240px";
+                let text = `
+                    <section style="background: ${color.colorA};">${color.colorA}</section>
+                    <section style="background: ${color.colorB};">${color.colorB}</section>
+                    <section style="background: ${color.colorC};">${color.colorC}</section>
+                    <section style="background: ${color.colorD};">${color.colorD}</section>
+                `;
+                li.innerHTML = text;
+                themepallete.appendChild(li);
+            })
+            numOfColors.innerHTML = `Number of Palletes is ${christmas.length}`;
+        }
+        else if (th == "Wedding") {
+            wedding.forEach((color) => {
+                let li = document.createElement("li");
+                li.style.width = "230px";
+                li.style.height = "240px";
+                let text = `
+                    <section style="background: ${color.colorA};">${color.colorA}</section>
+                    <section style="background: ${color.colorB};">${color.colorB}</section>
+                    <section style="background: ${color.colorC};">${color.colorC}</section>
+                    <section style="background: ${color.colorD};">${color.colorD}</section>
+                `;
+                li.innerHTML = text;
+                themepallete.appendChild(li);
+            })
+            numOfColors.innerHTML = `Number of Palletes is ${wedding.length}`;
+        }
+        else if (th == "Sky") {
+            sky.forEach((color) => {
+                let li = document.createElement("li");
+                li.style.width = "230px";
+                li.style.height = "240px";
+                let text = `
+                    <section style="background: ${color.colorA};">${color.colorA}</section>
+                    <section style="background: ${color.colorB};">${color.colorB}</section>
+                    <section style="background: ${color.colorC};">${color.colorC}</section>
+                    <section style="background: ${color.colorD};">${color.colorD}</section>
+                `;
+                li.innerHTML = text;
+                themepallete.appendChild(li);
+            })
+            numOfColors.innerHTML = `Number of Palletes is ${sky.length}`;
+        }
+        else if (th == "Dark") {
+            dark.forEach((color) => {
+                let li = document.createElement("li");
+                li.style.width = "230px";
+                li.style.height = "240px";
+                let text = `
+                    <section style="background: ${color.colorA};">${color.colorA}</section>
+                    <section style="background: ${color.colorB};">${color.colorB}</section>
+                    <section style="background: ${color.colorC};">${color.colorC}</section>
+                    <section style="background: ${color.colorD};">${color.colorD}</section>
+                `;
+                li.innerHTML = text;
+                themepallete.appendChild(li);
+            })
+            numOfColors.innerHTML = `Number of Palletes is ${dark.length}`;
+        }
         copyThemeHexCode();
     })
 })
